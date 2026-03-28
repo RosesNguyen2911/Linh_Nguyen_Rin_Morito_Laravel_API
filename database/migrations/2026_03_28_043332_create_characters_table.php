25aafeda6ec09fff61d3463cb0f62716beaa84b3
rin <rinoiod@gmail.com>
2026-03-28 00:46:32 -0400

    Created character table
    created character table migration

---
+++ database/migrations/2026_03_28_043332_create_characters_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCharactersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('characters', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('house')->nullable();
+            $table->bigInteger('movie_id')->nullable();
+            $table->string('character_image');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('characters');
+    }
+}
